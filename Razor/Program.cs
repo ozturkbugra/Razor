7b969d4d35d2e5c480742c34c303c2985e39e924
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-06-24 22:08:24 +0300

    yapılan değişiklikler ve sorgulamalar veri tabanına uyarlandı.

--- Razor/Program.cs
+++ Razor/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+builder.Services.AddTransient<IEmployeeRepository, SQLEmployeeRepository>();
 
 builder.Services.AddDbContext<DataContext>();
 var app = builder.Build();
---
+++ Razor/Repository/SQLEmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using Razor.Models;
+
+namespace Razor.Repository
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        private readonly DataContext _context;
+
+        public SQLEmployeeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee GetById(int id)
+        {
+            return _context.Employees.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Employee Update(Employee entity)
+        {
+            var entity2 = _context.Employees.FirstOrDefault(x => x.Id == entity.Id);
+            if(entity2 != null)
+            {
+                entity2.Name = entity.Name;
+                entity2.Email = entity.Email;
+                entity2.Department = entity.Department;
+                _context.SaveChanges();
+            }
+
+            return entity2;
+        }
+    }
+}
